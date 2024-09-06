1a06dc8ef26da0c86f3966f0f5dba866b29fb6c9
chi628 <miko82290@gmail.com>
2024-09-06 18:27:02 +0800

    first commit

---
+++ badge.js
@@ -0,0 +1,542 @@
+const badgeList = [
+  {
+    id: 'badge01',
+    title: '野營大廚師',
+    icon: './images/01-badge.webp',
+    blackIcon: './images/01-N-badge.webp',
+    layout: [
+      './images/layout_logo/01-layout-1.png',
+      './images/layout_logo/01-layout-2.png',
+      './images/layout_logo/01-layout-3.png',
+    ],
+    layout_noLogo: [
+      './images/layout_no_logo/01-layout-1.png',
+      './images/layout_no_logo/01-layout-2.png',
+      './images/layout_no_logo/01-layout-3.png',
+    ],
+  },
+  {
+    id: 'badge02',
+    title: '水上玩咖',
+    icon: './images/02-badge.webp',
+    blackIcon: './images/02-N-badge.webp',
+    layout: [
+      './images/layout_logo/02-layout-1.png',
+      './images/layout_logo/02-layout-2.png',
+      './images/layout_logo/02-layout-3.png',
+    ],
+    layout_noLogo: [
+      './images/layout_no_logo/02-layout-1.png',
+      './images/layout_no_logo/02-layout-2.png',
+      './images/layout_no_logo/02-layout-3.png',
+    ],
+  },
+  {
+    id: 'badge03',
+    title: '戶外露營家',
+    icon: './images/03-badge.webp',
+    blackIcon: './images/03-N-badge.webp',
+    layout: [
+      './images/layout_logo/03-layout-1.png',
+      './images/layout_logo/03-layout-2.png',
+      './images/layout_logo/03-layout-3.png',
+    ],
+    layout_noLogo: [
+      './images/layout_no_logo/03-layout-1.png',
+      './images/layout_no_logo/03-layout-2.png',
+      './images/layout_no_logo/03-layout-3.png',
+    ],
+  },
+  {
+    id: 'badge04',
+    title: '赤手登峰',
+    icon: './images/04-badge.webp',
+    blackIcon: './images/04-N-badge.webp',
+    layout: [
+      './images/layout_logo/04-layout-1.png',
+      './images/layout_logo/04-layout-2.png',
+      './images/layout_logo/04-layout-3.png',
+    ],
+    layout_noLogo: [
+      './images/layout_no_logo/04-layout-1.png',
+      './images/layout_no_logo/04-layout-2.png',
+      './images/layout_no_logo/04-layout-3.png',
+    ],
+  },
+  {
+    id: 'badge05',
+    title: '熱血勇跑家',
+    icon: './images/05-badge.webp',
+    blackIcon: './images/05-N-badge.webp',
+    layout: [
+      './images/layout_logo/05-layout-1.png',
+      './images/layout_logo/05-layout-2.png',
+      './images/layout_logo/05-layout-3.png',
+    ],
+    layout_noLogo: [
+      './images/layout_no_logo/05-layout-1.png',
+      './images/layout_no_logo/05-layout-2.png',
+      './images/layout_no_logo/05-layout-3.png',
+    ],
+  },
+  {
+    id: 'badge06',
+    title: '北面巔峰',
+    icon: './images/06-badge.webp',
+    blackIcon: './images/06-N-badge.webp',
+    layout: [
+      './images/layout_logo/06-layout-1.png',
+      './images/layout_logo/06-layout-2.png',
+      './images/layout_logo/06-layout-3.png',
+    ],
+    layout_noLogo: [
+      './images/layout_no_logo/06-layout-1.png',
+      './images/layout_no_logo/06-layout-2.png',
+      './images/layout_no_logo/06-layout-3.png',
+    ],
+  },
+  {
+    id: 'badge07',
+    title: '晴空之上',
+    icon: './images/07-badge.webp',
+    blackIcon: './images/07-N-badge.webp',
+    layout: [
+      './images/layout_logo/07-layout-1.png',
+      './images/layout_logo/07-layout-2.png',
+      './images/layout_logo/07-layout-3.png',
+    ],
+    layout_noLogo: [
+      './images/layout_no_logo/07-layout-1.png',
+      './images/layout_no_logo/07-layout-2.png',
+      './images/layout_no_logo/07-layout-3.png',
+    ],
+  },
+  {
+    id: 'badge08',
+    title: '山林對話',
+    icon: './images/08-badge.webp',
+    blackIcon: './images/08-N-badge.webp',
+    layout: [
+      './images/layout_logo/08-layout-1.png',
+      './images/layout_logo/08-layout-2.png',
+      './images/layout_logo/08-layout-3.png',
+    ],
+    layout_noLogo: [
+      './images/layout_no_logo/08-layout-1.png',
+      './images/layout_no_logo/08-layout-2.png',
+      './images/layout_no_logo/08-layout-3.png',
+    ],
+  },
+  {
+    id: 'badge09',
+    title: '手作體驗',
+    icon: './images/09-badge.webp',
+    blackIcon: './images/09-N-badge.webp',
+    layout: [
+      './images/layout_logo/09-layout-1.png',
+      './images/layout_logo/09-layout-2.png',
+      './images/layout_logo/09-layout-3.png',
+    ],
+    layout_noLogo: [
+      './images/layout_no_logo/09-layout-1.png',
+      './images/layout_no_logo/09-layout-2.png',
+      './images/layout_no_logo/09-layout-3.png',
+    ],
+  },
+  {
+    id: 'badge10',
+    title: '電車騎士',
+    icon: './images/10-badge.webp',
+    blackIcon: './images/10-N-badge.webp',
+    layout: [
+      './images/layout_logo/10-layout-1.png',
+      './images/layout_logo/10-layout-2.png',
+      './images/layout_logo/10-layout-3.png',
+    ],
+    layout_noLogo: [
+      './images/layout_no_logo/10-layout-1.png',
+      './images/layout_no_logo/10-layout-2.png',
+      './images/layout_no_logo/10-layout-3.png',
+    ],
+  },
+  {
+    id: 'badge11',
+    title: '探險攝影師',
+    icon: './images/11-badge.webp',
+    blackIcon: './images/11-N-badge.webp',
+    layout: [
+      './images/layout_logo/11-layout-1.png',
+      './images/layout_logo/11-layout-2.png',
+      './images/layout_logo/11-layout-3.png',
+    ],
+    layout_noLogo: [
+      './images/layout_no_logo/11-layout-1.png',
+      './images/layout_no_logo/11-layout-2.png',
+      './images/layout_no_logo/11-layout-3.png',
+    ],
+  },
+  {
+    id: 'badge12',
+    title: '精準導航員',
+    icon: './images/12-badge.webp',
+    blackIcon: './images/12-N-badge.webp',
+    layout: [
+      './images/layout_logo/12-layout-1.png',
+      './images/layout_logo/12-layout-2.png',
+      './images/layout_logo/12-layout-3.png',
+    ],
+    layout_noLogo: [
+      './images/layout_no_logo/12-layout-1.png',
+      './images/layout_no_logo/12-layout-2.png',
+      './images/layout_no_logo/12-layout-3.png',
+    ],
+  },
+]
+
+badgeList.forEach(badge => {
+  const imgList = [
+    badge.icon,
+    badge.blackIcon,
+    badge.layout[0],
+    badge.layout[1],
+    badge.layout[2],
+    badge.layout_noLogo[0],
+    badge.layout_noLogo[1],
+    badge.layout_noLogo[2],
+  ]
+
+  imgList.forEach(src => {
+    const img = new Image()
+    img.src = src
+  })
+})
+
+let selectedBadge
+
+document.addEventListener('DOMContentLoaded', () => {
+  const pageBadge = document.getElementById('page-badge')
+  const pageLayout = document.getElementById('page-layout')
+  const pageUploadImg = document.getElementById('page-uploadImg')
+  const pageShare = document.getElementById('page-share')
+
+  if (pageBadge) {
+    pageBadge.style.display = 'none'
+  }
+  if (pageLayout) {
+    pageLayout.style.display = 'none'
+  }
+  if (pageUploadImg) {
+    pageUploadImg.style.display = 'none'
+  }
+
+  if (pageShare) {
+    pageShare.style.display = 'none'
+  }
+
+  pageUploadImg.style.display = 'block'
+
+  const badge01Card = document.getElementById('badge01')
+  const badge02Card = document.getElementById('badge02')
+  const badge03Card = document.getElementById('badge03')
+  const badge04Card = document.getElementById('badge04')
+  const badge05Card = document.getElementById('badge05')
+  const badge06Card = document.getElementById('badge06')
+  const badge07Card = document.getElementById('badge07')
+  const badge08Card = document.getElementById('badge08')
+  const badge09Card = document.getElementById('badge09')
+  const badge10Card = document.getElementById('badge10')
+  const badge11Card = document.getElementById('badge11')
+  const badge12Card = document.getElementById('badge12')
+
+  if (badge01Card) {
+    badge01Card.addEventListener('click', () => {
+      selectedBadge = 'badge01'
+      tmp(selectedBadge)
+    })
+  }
+
+  if (badge02Card) {
+    badge02Card.addEventListener('click', () => {
+      selectedBadge = 'badge02'
+      tmp(selectedBadge)
+    })
+  }
+
+  if (badge03Card) {
+    badge03Card.addEventListener('click', () => {
+      selectedBadge = 'badge03'
+      tmp(selectedBadge)
+    })
+  }
+
+  if (badge04Card) {
+    badge04Card.addEventListener('click', () => {
+      selectedBadge = 'badge04'
+      tmp(selectedBadge)
+    })
+  }
+
+  if (badge05Card) {
+    badge05Card.addEventListener('click', () => {
+      selectedBadge = 'badge05'
+      tmp(selectedBadge)
+    })
+  }
+
+  if (badge06Card) {
+    badge01Card.addEventListener('click', () => {
+      selectedBadge = 'badge06'
+      tmp(selectedBadge)
+    })
+  }
+
+  if (badge07Card) {
+    badge07Card.addEventListener('click', () => {
+      selectedBadge = 'badge07'
+      tmp(selectedBadge)
+    })
+  }
+
+  if (badge08Card) {
+    badge08Card.addEventListener('click', () => {
+      selectedBadge = 'badge08'
+      tmp(selectedBadge)
+    })
+  }
+
+  if (badge09Card) {
+    badge09Card.addEventListener('click', () => {
+      selectedBadge = 'badge09'
+      tmp(selectedBadge)
+    })
+  }
+
+  if (badge10Card) {
+    badge10Card.addEventListener('click', () => {
+      selectedBadge = 'badge10'
+      tmp(selectedBadge)
+    })
+  }
+
+  if (badge11Card) {
+    badge11Card.addEventListener('click', () => {
+      selectedBadge = 'badge11'
+      tmp(selectedBadge)
+    })
+  }
+
+  if (badge12Card) {
+    badge12Card.addEventListener('click', () => {
+      selectedBadge = 'badge12'
+      tmp(selectedBadge)
+    })
+  }
+
+  const swiper = new Swiper('.layout-swiper', {
+    slidesPerView: 1.3,
+    centeredSlides: true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.layout-swiper-next',
+      prevEl: '.layout-swiper-prev',
+    },
+    on: {
+      init() {
+        const prevEl = document.querySelector('.layout-swiper-prev')
+
+        if (prevEl) {
+          prevEl.style.display = 'none'
+        }
+      },
+      slideChange() {
+        const prevEl = document.querySelector('.layout-swiper-prev')
+        const nextEl = document.querySelector('.layout-swiper-next')
+
+        if (this.activeIndex === 0) {
+          prevEl.style.display = 'none'
+        } else {
+          prevEl.style.display = 'flex'
+        }
+
+        if (this.activeIndex === this.slides.length - 1) {
+          nextEl.style.display = 'none'
+        } else {
+          nextEl.style.display = 'flex'
+        }
+      },
+    },
+  })
+
+  const layoutChooseBtn = document.getElementById('layout-choose-btn')
+  if (layoutChooseBtn) {
+    layoutChooseBtn.addEventListener('click', () => {
+      pageLayout.style.display = 'none'
+      pageUploadImg.style.display = 'block'
+    })
+  }
+
+  const layout_photo1 = document.getElementById('photo-1')
+  const layout_photo2 = document.getElementById('photo-2')
+  const layout_photo3 = document.getElementById('photo-3')
+
+  if (layout_photo1) {
+    layout_photo1.style.display = 'none'
+  }
+  if (layout_photo2) {
+    layout_photo2.style.display = 'none'
+  }
+  if (layout_photo3) {
+    layout_photo3.style.display = 'none'
+  }
+
+  layout_photo1.style.display = 'block'
+
+  const photo1 = document.getElementById('photo1')
+  const photo2Left = document.getElementById('photo-2-left')
+  const photo2Right = document.getElementById('photo-2-right')
+  const photo3Left = document.getElementById('photo-3-left')
+  const photo3TopRight = document.getElementById('photo-3-top-right')
+  const photo3BottomRight = document.getElementById('photo-3-bottom-right')
+
+  if (photo1) {
+    photo1.addEventListener('change', event => {
+      tmp2(event, 'photo-1-img', 'photo-1-cropper-btn')
+    })
+  }
+
+  if (photo2Left) {
+    photo2Left.addEventListener('change', event => {
+      tmp2(event, 'photo-2-left-img', 'photo-2-left-cropper-btn')
+    })
+  }
+
+  if (photo2Right) {
+    photo2Right.addEventListener('change', event => {
+      tmp2(event, 'photo-2-right-img', 'photo-2-right-cropper-btn')
+    })
+  }
+
+  if (photo3Left) {
+    photo3Left.addEventListener('change', event => {
+      tmp2(event, 'photo-3-left-img', 'photo-3-left-cropper-btn')
+    })
+  }
+
+  if (photo3TopRight) {
+    photo3TopRight.addEventListener('change', event => {
+      tmp2(event, 'photo-3-top-right-img', 'photo-3-top-right-cropper-btn')
+    })
+  }
+
+  if (photo3BottomRight) {
+    photo3BottomRight.addEventListener('change', event => {
+      tmp2(event, 'photo-3-bottom-right-img', 'photo-3-bottom-right-cropper-btn')
+    })
+  }
+
+  const photoUploadBtn = document.getElementById('photo-upload')
+
+  let base64Url
+  if (photoUploadBtn) {
+    photoUploadBtn.addEventListener('click', () => {
+      const bgImg = layout_photo1.querySelector('.bg-photo > img')
+      const canvas = document.createElement('canvas')
+      const context = canvas.getContext('2d')
+      const width = bgImg.getBoundingClientRect().width
+      const height = bgImg.getBoundingClientRect().height
+      canvas.width = width
+      canvas.height = height
+      context.imageSmoothingEnabled = true
+      context.drawImage(bgImg, 0, 0, width, height)
+
+      const allImages = layout_photo1.querySelectorAll('img')
+      const filteredImages = Array.from(allImages).filter(img => {
+        return !img.closest('.bg-photo')
+      })
+
+      filteredImages.forEach(img => {
+        const drawX = img.getBoundingClientRect().x - bgImg.getBoundingClientRect().x
+        const drawY = img.getBoundingClientRect().y - bgImg.getBoundingClientRect().y
+        context.drawImage(img, drawX, drawY, img.getBoundingClientRect().width, img.getBoundingClientRect().height)
+      })
+
+      base64Url = canvas.toDataURL()
+      console.log('base64', base64Url)
+      pageUploadImg.style.display = 'none'
+      pageShare.style.display = 'block'
+
+      document.getElementById('shard-img').src = base64Url
+    })
+  }
+
+  function tmp(id) {
+    const obj = badgeList.find(badge => badge.id === id)
+    console.log('obj', obj)
+
+    const swiper = document.querySelector('.swiper-wrapper')
+
+    for (let i = 0; i < obj.layout.length; i++) {
+      const slide = document.createElement('div')
+      slide.classList.add('swiper-slide')
+
+      const imgEl = document.createElement('img')
+      imgEl.src = obj.layout[i]
+
+      slide.appendChild(imgEl)
+      swiper.appendChild(slide)
+    }
+
+    if (pageBadge) {
+      pageBadge.style.display = 'none'
+    }
+
+    pageLayout.style.display = 'block'
+  }
+
+  function tmp2(evt, imgId, btnId) {
+    const image = document.getElementById(imgId)
+    const cropperBtn = document.getElementById(btnId)
+    const file = evt.target.files[0]
+    const reader = new FileReader()
+
+    console.log('img', image, cropperBtn)
+
+    reader.onload = function (e) {
+      image.src = e.target.result
+      const cropper = new Cropper(image, {
+        ready() {
+          console.log('crope', this.cropper, this.cropper.cropper)
+          if (this.cropper.cropper) {
+            this.cropper.cropper.style.zIndex = 20
+          }
+        },
+      })
+      if (cropperBtn) {
+        cropperBtn.style.display = 'flex'
+        cropperBtn.addEventListener('click', () => {
+          const croppedCanvas = cropper.getCroppedCanvas()
+          const roundedCanvas = getRoundedCanvas(croppedCanvas)
+          image.src = roundedCanvas.toDataURL()
+          cropperBtn.style.display = 'none'
+          cropper.destroy()
+        })
+      }
+    }
+    if (file) {
+      reader.readAsDataURL(file)
+    }
+  }
+
+  function getRoundedCanvas(sourceCanvas) {
+    var canvas = document.createElement('canvas')
+    var context = canvas.getContext('2d')
+    var width = sourceCanvas.width
+    var height = sourceCanvas.height
+
+    canvas.width = width
+    canvas.height = height
+    context.imageSmoothingEnabled = true
+    context.drawImage(sourceCanvas, 0, 0, width, height)
+    return canvas
+  }
+})
---
+++ index.js
@@ -0,0 +1,17 @@
+const badgeList = [
+  {
+    title: '野營大廚師',
+    icon: './images/01-badge.webp',
+    blackIcon: './images/01-N-badge.webp',
+    layout: [
+      './images/layout_logo/01-layout-1.png',
+      './images/layout_logo/01-layout-2.png',
+      './images/layout_logo/01-layout-3.png',
+    ],
+    layout_noLogo: [
+      './images/layout_no_logo/01-layout-1.png',
+      './images/layout_no_logo/01-layout-2.png',
+      './images/layout_no_logo/01-layout-3.png',
+    ],
+  },
+]
